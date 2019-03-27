5cc423f76fd1db33c8a6f00c28949a2bc0ea6504
moua2019 <47621564+moua2019@users.noreply.github.com>
2019-03-26 19:04:17 -0500

    test4 linda

--- view/Home.php
+++ view/Home.php
@@ -102,7 +102,7 @@
         <i class="fa fa-pinterest-p flip-hover-opacity"></i>
         <i class="fa fa-twitter flip-hover-opacity"></i>
         <i class="fa fa-linkedin flip-hover-opacity"></i>
-    </div>linda h. moua - test3
+    </div>linda h. moua - test4
 </footer>
 
 <script>
